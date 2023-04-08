95f7e9b01f2e2466fde395b132bb00c781b9224c
BasArthit <serverarthit@gmail.com>
2023-04-08 23:34:49 +0700

    Update Company.js

--- Company.js
+++ Company.js
@@ -2,32 +2,35 @@ class PayMoney {
     //ผลรวมของราคาสินค้า
     sumPrice() {
         let sum = 0;
-        let arr = [100,100, 300, 500];
+        let arr = [100, 100, 300, 500];
         for (let i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
         return sum;
     }
-    
-}
-const paym = new PayMoney();
-console.log(paym.sumPrice())
-const money = paym.sumPrice();
 
+}
+
+const paym = new PayMoney();
+const cusPay = new LevelCus();
+const money = paym.sumPrice();
+console.log(paym.sumPrice())
 let level;
 let giveVoucher;
 let specialOffer;
-if(money<1000){
+//ตรวจสอบว่าจะได้ Give Voucher กับ Special Offer เท่าไหร่
+if (money < 1000) {
     level = "Standard";
     giveVoucher = 30;
     specialOffer = 30;
-}else if(money>=1000 && money <=1200){
+} else if (money >= 1000 && money <= 1200) {
     level = "Luxury";
     giveVoucher = 40;
     specialOffer = 40;
-}else{
+} else {
     level = "Emerald";
     giveVoucher = 50;
     specialOffer = 50;
 }
-console.log(level,giveVoucher,specialOffer);
+
+console.log(level, giveVoucher, specialOffer);
