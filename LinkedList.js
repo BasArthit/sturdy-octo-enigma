5cc3d3ba750ab50681f840b71f035f306170b092
BasArthit <serverarthit@gmail.com>
2023-04-08 22:27:22 +0700

    Revert "Delete LinkedList.js"
    This reverts commit 346add902e8fde2b626b84b2b7a8014c5832e4c5.

---
+++ LinkedList.js
@@ -0,0 +1,17 @@
+class ListNode{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+class LinkedList{
+    constructor(head = null){
+        this.head = head;
+    }
+}
+let node1 = new ListNode(2);
+let node2 = new ListNode(6);
+
+node1.next = node2;
+let list = new LinkedList(node1);
+console.log(list);
