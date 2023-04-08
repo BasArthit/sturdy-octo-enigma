d07764e87afce812756be012beffb239108f78c3
BasArthit <serverarthit@gmail.com>
2023-04-08 20:35:59 +0700

    Create Animal.js

---
+++ Animal.js
@@ -0,0 +1,47 @@
+//คลาส Animal
+class Animal {
+    constructor(age, gender) {
+      this.age = age;
+      this.gender = gender;
+    }
+  
+    isMammal() {
+      //คำสั่งในเมธอด
+    }
+  
+    mate() {
+        //คำสั่งในเมธอด
+    }
+  }
+
+  class Duck extends Animal{
+    constructor(beakColor) {
+        this.beakColor = beakColor;
+      }
+      swim(){
+        //คำสั่งในเมธอด
+      }
+      quack(){
+        //คำสั่งในเมธอด
+      }
+  }
+
+  class Fish extends Animal{
+    constructor(sizeInFeet) {
+        this.sizeInFeet = sizeInFeet;
+      }
+      canEat(){
+        //คำสั่งในเมธอด
+      }
+     
+  }
+
+  class Fish extends Animal{
+    constructor(sizeInFeet) {
+        this.sizeInFeet = sizeInFeet;
+      }
+      canEat(){
+        //คำสั่งในเมธอด
+      }
+     
+  }
